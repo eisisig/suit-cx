79897f89a0d3b879c7d035ac1214f3397fdd653f
Eisi Sig <eisi@meniga.is>
2016-09-16 14:05:47 +0000

    Allowing first parameter to be a string for name

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import bemClassNames from 'bem-classnames';
 
 const suitCx = ( settings, params = {}, className = {} ) => {
 
+	// If you only provide string it is the name of the main class
+	if ( typeof settings === 'string' ) {
+		settings = { name: settings }
+	}
+
 	const classes = bemClassNames(settings, params, className);
 	const parentName = settings.name;
 
